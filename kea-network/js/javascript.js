1386dc43b7a3ad6b4f9d1f02a1cbd7e4a399691a
needrent <sej@needrent.dk>
2018-12-13 17:37:07 +0100

    War, who yeah! What is it good for?

--- kea-network/js/javascript.js
+++ kea-network/js/javascript.js
@@ -2,7 +2,7 @@
 let loginPage = document.getElementById("loginPage");
 let loginBtn = document.getElementById("loginBtn");
 let logoutBtn = document.getElementById("logoutBtn");
-
+    loginPage.style.display = "none";// Save with cookie and delete
 loginBtn.addEventListener("click", login);
 
 function login(){
@@ -35,3 +35,40 @@ function closeNav(){
     menuBtn.removeEventListener("click",closeNav);
 
 }
+//Chat system
+let chatWindow = document.getElementById("chatWindow");
+
+function getChat(){
+    let createDiv = document.createElement("div");
+    createDiv.className = "ChatOut";
+
+    let node = document.createTextNode("Hello World.");
+
+
+    createDiv.appendChild(node);
+
+
+
+    var element = document.getElementById("chatWindow");
+    element.appendChild(createDiv);
+
+}
+
+
+    /*    function GetValue () {
+            var area = document.getElementById ("myArea");
+            alert (area.value);
+        }
+        function SetValue () {
+            var area = document.getElementById ("myArea");
+            area.value = "The new value";
+        }
+    </script>
+</head>
+<body>
+    <button onclick="GetValue ();">Get the velue of the textarea!</button>
+    <br />
+    <button onclick="SetValue ();">Modify the value of the textarea!</button>
+    <br />
+    <textarea id="myArea"/>The contents of the textarea</textarea>
+</body>*/
